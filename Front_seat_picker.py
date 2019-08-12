bc816998de7b7572ebabd4b41287b2d6ef6df077
Sjos12 <42643250+Sjos12@users.noreply.github.com>
2019-08-12 12:35:57 +0200

    First version
    First version of the front seat picker

---
+++ Front_seat_picker.py
@@ -0,0 +1,14 @@
+
+kids = ["giselle", "ally-ann", "esperanza", "denzel"]
+
+who_is_next = input("Hi there!, Who was in the front seat the last time?")
+
+if who_is_next.lower() == kids[0]:
+    print("It's Ally-Ann's turn!")
+elif who_is_next.lower()== kids[1]:
+    print("It's Esperanza's turn!")
+elif who_is_next.lower() == kids[2]:
+    print("It's Denzel's turn!")
+elif who_is_next.lower() == kids[3]:
+    print("It's Giselle's turn!")
+
